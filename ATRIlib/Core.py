c41bf1f82a94e363edf38faa399c552959f1986a
Ohdmire <38505121+Ohdmire@users.noreply.github.com>
2024-04-09 10:03:52 +0800

    update

--- ATRIlib/Core.py
+++ ATRIlib/Core.py
@@ -1,6 +1,9 @@
 from ppyapiv2 import PPYdata
+from mongodb import MongoDB
 
 import asyncio
+
+import json
 
 
 class ATRICore:
@@ -8,14 +11,45 @@ class ATRICore:
         self.ppy=PPYdata()
         self.ppy.get_token()
 
+        self.db_user=MongoDB('localhost',27017,'osu','user') #数据库名字为osu，表名为user
+        self.db_beatmap=MongoDB('localhost',27017,'osu','beatmap') #数据库名字为osu，表名为beatmap
 
-    def get_user_id(self, osuname):
-        return self.ppy.get_user_id(osuname)
+    
+    # 更新玩家信息
+    def update_user_info(self,osuname):
+        userdata=self.ppy.get_user_info(osuname)
+
+        self.db_user.update(
+            {"id": userdata["id"]},  # 查询条件
+            {"$set": userdata},  # 插入的数据
+            upsert=True  # 如果不存在则插入
+            ) 
+        
+    def update_bplist_info(self,osuname):
+        userdata=self.ppy.get_user_info(osuname)
+
+        userid=userdata['id']
+        data=self.ppy.get_user_best_all_info(userid)
+
+        for i in data:
+            self.db_user.update(
+                {"id": i["id"]},  # 查询条件
+                {"$set": i},  # 插入的数据
+                upsert=True  # 如果不存在则插入
+                )
+    
+
+#score combo acc mods time 
+
+
 
 
 a=ATRICore()
 
+a.update_user_info('ATRI1024')
 
-id=a.get_user_id('ATRI1024')
 
-print(id)
+a.update_bplist_info('ATRI1024')
+b=a.db_user.find({'username':'ATRI1024'})
+for i in b:
+    print(i)
--- ATRIlib/ppyapiv2.py
+++ ATRIlib/ppyapiv2.py
@@ -1,6 +1,7 @@
 from Config import Config
 
 import requests
+import json
 
 
 
@@ -23,19 +24,86 @@ class PPYdata:
         self.token = response.json()['access_token']
 
 
-    
     # 获取玩家信息id
-    def get_user_id(self,osuname):
+    def get_user_info(self,osuname):
         token = self.token
         url = f'https://osu.ppy.sh/api/v2/users/{osuname}?key=username'
         headers = {'Authorization': f'Bearer {token}'}
-        
         response = requests.get(url, headers=headers)
-        return response.json()
+
+        data = response.json()
+
+        id=data['id']
+
+        username=data['username']
+
+        global_rank=data['statistics']['global_rank']
+        country_rank=data['statistics']['country_rank']
+
+        join_date=data['join_date']
+
+        country_code=data['country']['code']
+
+        pp=data['statistics']['pp']
+
+        hit_accuracy=data['statistics']['hit_accuracy']
+
+        play_count=data['statistics']['play_count']
+        play_time=data['statistics']['play_time']
+
+        ranked_score=data['statistics']['ranked_score']
+        total_score=data['statistics']['total_score']
+
+        total_hits=data['statistics']['total_hits']
+        maximum_combo=data['statistics']['maximum_combo']
+
+
+        ss_count=data['statistics']['grade_counts']['ss']
+        s_count=data['statistics']['grade_counts']['s']
+        a_count=data['statistics']['grade_counts']['a']
+        ssh_count=data['statistics']['grade_counts']['ssh']
+        sh_count=data['statistics']['grade_counts']['sh']
+
+        document={'id':id,'username':username,'global_rank':global_rank,'country_rank':country_rank,'join_date':join_date,'country_code':country_code,'pp':pp,'hit_accuracy':hit_accuracy,'play_count':play_count,'play_time':play_time,'ranked_score':ranked_score,'total_score':total_score,'total_hits':total_hits,'maximum_combo':maximum_combo,'ss_count':ss_count,'s_count':s_count,'a_count':a_count,'ssh_count':ssh_count,'sh_count':sh_count}
+        
+        return document
+
   
                 
+    def get_user_best_all_info(self,userid):
+        doucument_list=[]
+        token = self.token
+        url = f'https://osu.ppy.sh/api/v2/users/{userid}/scores/best?mode=osu&limit=100'
+        headers = {'Authorization': f'Bearer {token}'}
+        
+        response = requests.get(url, headers=headers)
 
+        data = response.json()
 
+        count=1
+        for i in data:
+            id=i['user']['id']
+            username=i['user']['username']
+
+            beatmap_id=i['beatmap']['id']
+
+            rank=i['rank']
+            perfect=i['perfect']
+
+            score=i['score']
+            combo=i['max_combo']
+            acc=i['accuracy']
+            mods=i['mods']
+            if mods==[]:
+                mods=['NM']
+            time=i['created_at']
+
+            bp_index=str(f'bp{count}')
+            count+=1
+
+            doucument_list.append({'id':id,'username':username,bp_index:{'beatmap_id':beatmap_id,'rank':rank,'perfect':perfect,'score':score,'combo':combo,'acc':acc,'mods':mods,'time':time}})
+
+        return doucument_list
         
 
-    
+
