7b563bdd72020184ff6e9a8fcd85f867769d4074
Ohdmire <38505121+Ohdmire@users.noreply.github.com>
2024-10-13 02:51:56 +0800

    test limited

--- ATRIlib/DRAW/draw_profile.py
+++ ATRIlib/DRAW/draw_profile.py
@@ -131,7 +131,7 @@ async def html_to_image(html_string, max_img_width=1400, max_img_height=800, max
         .bbcode-spoilerbox__body {{
             display: block;  /* 默认显示内容 */
             margin-top: 10px;
-            padding-left: 20px;
+            padding-left: 40px;
         }}
 
         .bbcode-spoilerbox__link {{
@@ -231,7 +231,7 @@ async def html_to_image(html_string, max_img_width=1400, max_img_height=800, max
     # 压缩PNG文件并转换为JPEG
     with Image.open(png_output_path) as img:
         # 确保图片尺寸不超过65000x65000
-        max_size = 65000
+        max_size = 12000
         if img.width > max_size or img.height > max_size:
             ratio = min(max_size / img.width, max_size / img.height)
             new_size = (int(img.width * ratio), int(img.height * ratio))
