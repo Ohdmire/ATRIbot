9da200e0d675758632e3d14ab22e88783e6648e0
Ohdmire <38505121+Ohdmire@users.noreply.github.com>
2024-12-20 19:19:25 +0800

    Update profile.py

--- ATRIlib/profile.py
+++ ATRIlib/profile.py
@@ -3,11 +3,19 @@ from ATRIlib.DB.Mongodb import db_user,db_yesterday
 
 async def calculate_profile(user_id,is_yesterday=False):
 
+    block_key_list = ["www.z4a.net"]
+
     userstruct = db_user.find_one({"id": user_id})
 
     username = userstruct["username"]
     avatar_url = userstruct["avatar_url"]
     html_content = userstruct["page"]["html"]
+    raw_content = userstruct["page"]["raw"]
+
+    # 检查是否出现敏感词
+    for block_key in block_key_list:
+        if block_key in html_content:
+            raise ValueError("该profile存在可疑网址，请检查")
 
     if is_yesterday:
         userstruct = db_yesterday.find_one({"user_id": user_id})
