4679f4db003326a54a5e02386d90278d590765d8
Ohdmire <38505121+Ohdmire@users.noreply.github.com>
2024-12-31 14:03:29 +0800

    绑定提示词

--- ATRIlib/bind.py
+++ ATRIlib/bind.py
@@ -12,13 +12,13 @@ async def update_bind_info(qq_id,osuname):
         update_user(userdata)
         return update_bind(qq_id, userdata)
     else:
-        raise ValueError(f'无法在ppy中找到{osuname}')
+        raise ValueError(f'无法在ppy的数据库中找到{osuname}')
 
 # 通过bind数据库获取userstruct
 def get_userstruct_from_bind(qq_id):
     bindstruct = find_bind(qq_id)
     if bindstruct is None:
-        raise ValueError(f'无法在数据库中找到{qq_id}绑定的对象\n尝试输入!getbind osuname')
+        raise ValueError(f'无法在数据库中找到qq={qq_id}绑定的对象\n尝试输入 !getbind')
     else:
         userstruct = db_user.find_one({'id': bindstruct["user_id"]})
         return userstruct
@@ -26,7 +26,7 @@ def get_userstruct_from_bind(qq_id):
 async def get_userstrct_update_from_bind(qq_id):
     userstruct = get_userstruct_from_bind(qq_id)
     if userstruct is None:
-        raise ValueError(f'无法在数据库中找到{qq_id}绑定的对象\n尝试输入!getbind osuname')
+        raise ValueError(f'无法在数据库中找到qq={qq_id}绑定的对象\n尝试输入 !getbind')
     else:
         userstruct = await update_user_info(userstruct['username'])
         return userstruct
